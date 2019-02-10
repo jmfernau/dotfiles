6b4cec8a5333ed4da15d72a17418c8f022aa38dd
Jon Fernau <jfernau@jmf-linux.san.rr.com>
2019-02-09 21:46:11 -0800

    added automatice scripts for chruby and ruby-install

---
+++ scripts/ruby-install.sh
@@ -0,0 +1,4 @@
+wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz && \
+tar -xzvf ruby-install-0.7.0.tar.gz && \
+cd ruby-install-0.7.0/ && \
+sudo make install
