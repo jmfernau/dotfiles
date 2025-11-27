c937e1485ec5d2667db55b329c6090e05143109f
Jonathan Fernau <jmfernau@gmail.com>
2025-11-27 10:01:56 -0500

    chore: remove unused files

--- check-pry.sh
+++
@@ -1,1 +0,0 @@
-git grep --untracked "pry"
--- scripts/chruby-install.sh
+++
@@ -1,4 +0,0 @@
-wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz && \
-tar -xzvf chruby-0.3.9.tar.gz && \
-cd chruby-0.3.9/ && \
-sudo make install
